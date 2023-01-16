930441c6661e4463b37ff5ac58aa6373739aed42
unknown <espinabrian@gmail.com>
2023-01-16 15:48:47 +0800

    added route to get all todos

--- server/index.js
+++ server/index.js
@@ -1,20 +1,26 @@
 const express = require("express");
-const cors = require("cors");
-const PORT = 3000;
-
 // initialize app
 const app = express();
+const cors = require("cors");
+const pool =  require("./db");
+const port = 3080;
+
 
 //middlewares
 app.use(cors());
 app.use(express.json());
 
 //routes
-app.get('/', (req, res) => {
-    res.send("Root");
+
+//todos get all todos 
+app.get('/todos', async (req, res) => {
+    const todos = await pool.query(
+        'SELECT * FROM todos'
+    )
+    res.json(todos.rows)
 })
 
 
-app.listen(PORT, ()=>{
-    console.log(`listening on PORT ${PORT}`);
+app.listen(port, ()=>{
+    console.log(`listening on PORT ${port}`);
 })
